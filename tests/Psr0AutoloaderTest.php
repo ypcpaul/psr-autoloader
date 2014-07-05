5672f8d935594dc455b4aba2abf2ccf2b7e2cd38
Paul Cayaco <mives29@gmail.com>
2014-07-05 19:08:49 +0800

    Add unit test for psr0autoload

---
+++ tests/Psr0AutoloaderTest.php
@@ -0,0 +1,31 @@
+<?php
+include __DIR__ . '/../src/psr0autoload.php';
+class Psr0AutoloaderTest extends PHPUnit_Framework_TestCase
+{
+    public static function setupBeforeClass() 
+    {
+        mkdir(__DIR__ . '/sample1/sample2', 0777, true);
+        $content = "<?php
+        namespace sample1\sample2;
+        
+        class TestClass {
+        public static function testFunctionPleaseDelete() { return true; }
+        }
+        ";
+        file_put_contents(__DIR__ . '/sample1/sample2/TestClass.php', $content);
+    }
+
+    public function testAutoload() 
+    {
+        tunalaruan\psr\psr0autoload(__DIR__);
+        sample1\sample2\TestClass::testFunctionPleaseDelete();
+        $this->assertTrue(class_exists('sample1\sample2\TestClass'));
+    }
+
+    public static function tearDownAfterClass() 
+    {
+        unlink(__DIR__ . '/sample1/sample2/TestClass.php');
+        rmdir(__DIR__ . '/sample1/sample2/');
+        rmdir(__DIR__ . '/sample1/');
+    }
+}
